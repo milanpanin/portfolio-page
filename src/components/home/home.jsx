aefd80d80b3cefa09796f3bd8d74f1d9e7918cd1
milanpanin <milanpanin@gmail.com>
2021-07-03 04:58:33 +0200

    Adding social icons

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -8,6 +8,11 @@ const Home = () => {
                 Milan is a student from Novi Sad / Serbia, who likes to spend his time exploring the 
                 interesting world of JavaScript and learning how to develop the web in the best ways!
             </p>
+            <div className="header-social">
+                <i className="fa fa-linkedin-square" aria-hidden="true"></i>
+                <i className="fa fa-github" aria-hidden="true"></i>
+                <i className="fa fa-envelope" aria-hidden="true"></i>
+            </div>
         </section>
     );
 }
