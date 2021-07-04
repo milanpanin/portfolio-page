9b4a542a5721269b4cd6208bab57b4c070f27399
milanpanin <milanpanin@gmail.com>
2021-07-05 01:20:59 +0200

    Update Blog page

--- src/components/Blog/blog.jsx
+++ src/components/Blog/blog.jsx
@@ -4,7 +4,11 @@ const Blog = () => {
     return ( 
         <section className="blog">
             <h1 className="blog-header">Blog</h1><br/>
-            <p className="blog-info">Coming soon... :)</p>
+            <p className="blog-info">
+                <span>Coming soon...</span><br/>
+                Until then, read my blog on the Medium service: <a target="_blank" rel="noopener noreferrer" href="https://medium.com/@milanpanin">https://medium.com/@milanpanin</a><br/>
+                I hope you like my articles, and that you learned something new from them :)
+            </p>
         </section>
      );
 }
