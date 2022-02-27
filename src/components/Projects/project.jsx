7d1fb4988d5c9f7da8ce091d05030d9099887fd0
Milan <milanpanin@gmail.com>
2022-02-28 00:47:37 +0100

    Update

--- src/components/Projects/project.jsx
+++ src/components/Projects/project.jsx
@@ -6,7 +6,7 @@ const Project = (props) => {
             <h2 className="project-name">{props.project.name}</h2>
             <p className="project-type">Project type: {props.project.projectType}</p>
             <p className="project-description">Project description: {props.project.description}</p>
-            <p className="project-tech">Used technologys: {props.project.technologys.map(item => `${item} `)}</p>
+            <p className="project-tech">Used technologies: {props.project.technologys.map(item => `${item} `)}</p>
             <p className="project-date">Project created: {props.project.date}</p>
             <a href={props.project.source} target="_blank" rel="noopener noreferrer"><button className="project-visit">Visit project</button></a> 
         </section>
